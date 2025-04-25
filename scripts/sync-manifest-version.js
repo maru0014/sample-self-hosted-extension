d575606c39305e2f182bf58384f81bd223b74475
maru0014 <maru0014@gmail.com>
2025-04-25 16:38:36 +0900

    マニフェストパスの修正

--- scripts/sync-manifest-version.js
+++ scripts/sync-manifest-version.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const pkg = require("./../package.json");
-const manifestPath = "./.output/chrome-mv3/manifest.json";
-
+const manifestPath = "../.output/chrome-mv3/manifest.json";
 const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf-8"));
 manifest.version = pkg.version;
 fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2) + "\n");
