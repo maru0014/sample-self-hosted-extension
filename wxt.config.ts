12de8b10002876f6223fece9dd4ee9f4937c2351
maru0014 <maru0014@gmail.com>
2025-04-25 17:16:28 +0900

    deploy: updates.xml を生成するステップを追加し、バージョンを 1.0.0 に更新

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  manifest: {
+    update_url: 'https://maru0014.github.io/sample-self-hosted-extension/updates.xml',
+  },
+});
